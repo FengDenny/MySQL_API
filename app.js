790b116e998658bc8f05c2c5dc87b9aa2654447f
FengDenny <fengdennyy@gmail.com>
2021-02-09 16:58:04 -0800

    express-session/ express-mysql-session

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const morgan = require("morgan");
 const usersRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const authRoute = require("./routes/auth");
+const dbConfig = require("./config/dbConfig.js");
 // express app initialized
 const app = express();
+// make a session
+const sessions = require("express-session");
+const mysqlSessions = require("express-mysql-session")(sessions);
 
 // Body Parser START
 app.use(express.json({ limit: "10kb" }));
@@ -17,7 +21,17 @@ if (process.env.NODE_ENV === "development") {
 } else if (process.env.NODE_ENV === "production") {
   app.use(morgan("prod"));
 }
-
+// session table creation for MySQL
+const mysqlSessionStore = new mysqlSessions({}, dbConfig);
+app.use(
+  sessions({
+    key: "mysql_api_id",
+    secret: "sf6s56sf65sd5f6sf1sd6f1sd",
+    store: mysqlSessionStore,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 // Routes
 app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/posts", postRoute);
